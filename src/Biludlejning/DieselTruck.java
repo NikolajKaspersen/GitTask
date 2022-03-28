3a8d0b7512006f539e345995f71a0e140f869a40
NikolajKaspersen <99389341+NikolajKaspersen@users.noreply.github.com>
2022-03-28 11:29:22 +0200

    Init

--- src/Biludlejning/DieselTruck.java
+++ src/Biludlejning/DieselTruck.java
@@ -4,5 +4,5 @@ public class DieselTruck extends DieselCar{
 
     public DieselTruck(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre, boolean hasParticlefilter) {
         super(registrationNumber, make, model, numberOfDoors, kmPrLitre, hasParticlefilter);
-    }
+    }//fsaf
 }
